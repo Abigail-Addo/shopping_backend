d919255d8dcb38978a118b8856629ef3770ae4cb
abigail <abigailadwoaaddo@gmail.com>
2024-08-12 17:03:25 +0000

    Update

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,50 +1,52 @@
 // Update with your config settings.
-require('dotenv').config();
+require("dotenv").config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-export const development = {
-  client: 'mysql',
-  connection: {
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    port: process.env.SQL_PORT,
-    database: process.env.SQL_DATABASE,
-    host: process.env.SQL_HOST,
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      port: process.env.SQL_PORT,
+      database: process.env.SQL_DATABASE,
+      host: process.env.SQL_HOST,
+    },
+    seeds: {
+      directory: "./seed",
+    },
   },
-  seeds: {
-    directory: './seed'
-  }
-};
-export const staging = {
-  client: 'mysql',
-  connection: {
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    port: process.env.SQL_PORT,
-    database: process.env.SQL_DATABASE,
-    host: process.env.SQL_HOST,
+  staging: {
+    client: "mysql",
+    connection: {
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      port: process.env.SQL_PORT,
+      database: process.env.SQL_DATABASE,
+      host: process.env.SQL_HOST,
+    },
+    seeds: {
+      directory: "./seed",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
-  seeds: {
-    directory: './seed'
+  production: {
+    client: "mysql",
+    connection: {
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      port: process.env.SQL_PORT,
+      database: process.env.SQL_DATABASE,
+      host: process.env.SQL_HOST,
+    },
+    seeds: {
+      directory: "./seed",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
-export const production = {
-  client: 'mysql',
-  connection: {
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    port: process.env.SQL_PORT,
-    database: process.env.SQL_DATABASE,
-    host: process.env.SQL_HOST,
-  },
-  seeds: {
-    directory: './seed'
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
 };
