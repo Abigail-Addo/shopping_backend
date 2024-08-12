ca09a58d453cd845579192bd91f7b35cdf8d09f0
abigail <abigailadwoaaddo@gmail.com>
2024-08-12 16:48:51 +0000

    Update

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,63 +1,48 @@
 // Update with your config settings.
-
+require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
-
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   },
-  //   useNullAsDefault: true, // Required for SQLite
-  // },
-  
-  development: {
-    client: 'mysql',
-    connection: {
-      user: 'avnadmin',
-      password: 'AVNS_bYjtQ-QU-cdAdbH6E-j',
-      port: '23478',
-      database: 'defaultdb',
-      host: 'hairbie-shop-addoa350-448a.l.aivencloud.com',
-    },
-    seeds: {
-      directory: './seed'
-    }
+export const development = {
+  client: 'mysql',
+  connection: {
+    user: 'avnadmin',
+    password: process.env.SQL_PASSWORD,
+    port: '23478',
+    database: 'defaultdb',
+    host: 'hairbie-shop-addoa350-448a.l.aivencloud.com',
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'mysql',
-    connection: {
-      user: 'avnadmin',
-      password: 'AVNS_bYjtQ-QU-cdAdbH6E-j',
-      port: '23478',
-      database: 'defaultdb',
-      host: 'hairbie-shop-addoa350-448a.l.aivencloud.com',
-    },
-    seeds: {
-      directory: './seed'
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: './seed'
   }
-
+};
+export const staging = {
+  client: 'postgresql',
+  connection: {
+    database: 'my_db',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
+export const production = {
+  client: 'postgresql',
+  connection: {
+    database: 'my_db',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
